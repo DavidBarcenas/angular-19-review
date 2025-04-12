8d26520432e4dc20dce451f8f1efd6e2d52d442a
David Bárcenas <dbarcenas.dev@gmail.com>
2025-04-11 22:00:05 -0700

    Configuracion de prettier, husky y lint staged

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,8 +5,11 @@ import { RouterOutlet } from '@angular/router';
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'ng-review';
+  constructor() {
+    console.log('AppComponent initialized');
+  }
 }
