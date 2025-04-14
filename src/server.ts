60642e024a75f8b6ad4f364b9caa3883ba2a97a9
David Bárcenas <dbarcenas.dev@gmail.com>
2025-04-14 15:57:23 -0700

    Ajuste de prettier para html

--- src/server.ts
+++ src/server.ts
@@ -54,6 +54,7 @@ app.use('/**', (req, res, next) => {
 if (isMainModule(import.meta.url)) {
   const port = process.env['PORT'] || 4000;
   app.listen(port, () => {
+    // eslint-disable-next-line no-console
     console.log(`Node Express server listening on http://localhost:${port}`);
   });
 }
